4365af5ed53933ea3441c6510f810016c0a53996
poko <pet.pokorny@gmail.com>
2024-08-02 11:36:25 +0200

    Dockerize backend.

--- amcef_proj/settings.py
+++ amcef_proj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,7 +83,16 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('POSTGRES_DB', 'mydatabase'),
+#         'USER': os.getenv('POSTGRES_USER', 'myuser'),
+#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'mypassword'),
+#         'HOST': 'db',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -124,3 +134,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Define the path for collected static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
